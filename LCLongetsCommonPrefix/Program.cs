99aebe78d9cf1a8a97f5636bd80e6c2c35f2be1a
jeremy_plott <plott.j@pg.com>
2020-01-27 12:35:45 -0500

    Add project files.

---
+++ LCLongetsCommonPrefix/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace LCLongetsCommonPrefix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] strs = new string[] { };
+
+
+            Console.WriteLine(LongestCommonPrefix(strs));
+
+            string LongestCommonPrefix(string[] strs)
+            {
+                if (!strs.Any())
+                {
+                    return string.Empty;
+                }
+                    
+                var longestSubstring = string.Empty;
+
+                var shortestString = strs.OrderBy(x => x.Length).First();
+
+                var index = 0;
+
+                while (index < shortestString.Length)
+                {
+                    var letter = shortestString[index];
+
+                    if (!strs.All(x => x[index].Equals(letter)))
+                    {
+                        return longestSubstring;
+                    }
+                    longestSubstring += letter;
+                    index++;
+                }
+
+                return longestSubstring;
+            }
+        }
+    }
+}
